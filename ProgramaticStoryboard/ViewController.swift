3c2f541f470201617f391f433ec5e471fd892743
AtomicSwift <nishad.dev@icloud.com>
2020-04-03 01:45:34 +0530

    programatic UI View Controller

--- ProgramaticStoryboard/ViewController.swift
+++ ProgramaticStoryboard/ViewController.swift
@@ -13,9 +13,62 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
+        
+        view.addSubview(skullImageView)
+        view.addSubview(introTextView)
+        
+        setupLayout()
     }
 
+
+    //Background image used in the viewController
+    let skullImageView: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "skullandflower"))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    let introTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Join our partner program today"
+        textView.font = UIFont.boldSystemFont(ofSize: 18)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    let topContainerView: UIView = {
+        let topImageContainerView = UIView()
+        topImageContainerView.backgroundColor = .blue
+        //topImageContainerView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        return topImageContainerView
+    }()
+    
+    func setupLayout()  {
+
+         view.addSubview(topContainerView)
+         topContainerView.translatesAutoresizingMaskIntoConstraints = false
+         topContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+         topContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+         topContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+         topContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        topContainerView.addSubview(skullImageView)
+         skullImageView.translatesAutoresizingMaskIntoConstraints = false
+         skullImageView.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor).isActive = true
+         skullImageView.centerYAnchor.constraint(equalTo: topContainerView.centerYAnchor).isActive = true
+//         skullImageView.topAnchor.constraint(equalTo: topContainerView.topAnchor).isActive = true
+         skullImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+         skullImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+         introTextView.topAnchor.constraint(equalTo: skullImageView.bottomAnchor, constant: 10).isActive = true
+         introTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+         introTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+         introTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+    }
+    //End of background image
 
 }
 
